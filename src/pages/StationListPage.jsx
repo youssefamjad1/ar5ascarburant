e40fda63e8322b0bdb98a59db13e367939c5092a
Youssef HIMMI <yhimmi1@gmail.com>
2025-04-11 19:41:06 +0200

    WIP: before rebase

--- src/pages/StationListPage.jsx
+++ src/pages/StationListPage.jsx
@@ -2,22 +2,34 @@ import React, { useEffect, useState } from 'react';
 import { List, ListItem, ListItemText, Typography } from '@mui/material';
 import axios from 'axios';
 
+const API_URL = import.meta.env.VITE_API_URL;
+
 function StationListPage() {
   const [stations, setStations] = useState([]);
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition((position) => {
-      const { latitude, longitude } = position.coords;
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
 
-      // Use the correct API URL deployed on Vercel
-      axios.get(`https://ar5ascarburant-la3rktt00-youssefs-projects-81e32e79.vercel.app/api/stations?lat=${latitude}&lng=${longitude}`)
-        .then(response => {
-          setStations(response.data);
-        })
-        .catch(error => {
-          console.error("Error fetching stations: ", error);
-        });
-    });
+        axios
+          .get(`${API_URL}/api/stations?lat=${latitude}&lng=${longitude}`)
+          .then((response) => {
+            setStations(response.data);
+          })
+          .catch((error) => {
+            console.error('Error fetching stations: ', error);
+          });
+      },
+      (error) => {
+        console.error('Error getting user location: ', error);
+      },
+      {
+        enableHighAccuracy: true,
+        timeout: 10000,
+        maximumAge: 0,
+      }
+    );
   }, []);
 
   return (
@@ -26,7 +38,7 @@ function StationListPage() {
         Nearest Stations
       </Typography>
       <List>
-        {stations.map(station => (
+        {stations.map((station) => (
           <ListItem key={station._id}>
             <ListItemText
               primary={`${station.name} - Gazoil: ${station.gazoilPrice} DH - Diesel: ${station.dieselPrice} DH`}
